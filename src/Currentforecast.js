335708de13361c5335b272107e55d5de448c4bcb
FannySieminski <106988505+FannySieminski@users.noreply.github.com>
2022-08-30 12:55:19 +0200

    Forecast added

--- src/Currentforecast.js
+++ src/Currentforecast.js
@@ -1,18 +1,41 @@
-import React, { usestate } from "react";
+import React, { useState } from "react";
 import "./Currentforecast.css";
 import axios from "axios";
 
 export default function Currentforecast() {
+  let [city, setCity] = useState("");
+  let [temperature, setTemperature] = useState("");
+  let [description, setDescription] = useState("");
+  let [humidity, setHumidity] = useState("");
+  let [wind, setWind] = useState("");
+  let [icon, setIcon] = useState("");
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c4d36cc9101ca41ceee2cff31c436ac9&units=metric`;
+  axios.get(url).then(showWeather);
+  function showWeather(response) {
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon
+      (`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
+  } 
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+ 
   return (
     <div>
       <div className="enter-city">
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             className="city-entered"
             placeholder="Enter a city..."
             autofocus="on"
             autocomplete="off"
+            onChange={updateCity}
           />
           <input className="submit-button" type="submit" value="Search" />
         </form>
@@ -20,37 +43,36 @@ export default function Currentforecast() {
       
       <div className="citytime">
         {" "}
-        <p className="currentcity"> Paris </p>
+        <p className="currentcity"> {city} </p>
         <p className="currenttime"> Saturday, 8:30 </p>
       </div>
       <div className="card mb-3 central-card">
         <div className="row g-0">
           <div className="col-md-2">
-            <span className="icon" role="img" aria-label="icon">
-              ☀️
-            </span>
+            <img className="icon" src={icon} alt="weather"
+            />
           </div>
           <div className="col-md-3">
             <div className="card-body">
               <div className="temperature">
-                <h4>37</h4> <span className="units">ºC</span>{" "}
+                <h4>{Math.round(temperature)}</h4> <span className="units">ºC</span>{" "}
               </div>
             </div>
           </div>
           <div class="col-md-7">
             <ul class="current-weather">
               <li>
-                Weather: <span className="description">Sunny</span>
+                Weather: <span className="description">{description}</span>
               </li>
               <li>
-                Humidity: <span className="humidity"> </span>{" "}
-                <strong>4 %</strong>
+                Humidity: <span className="humidity"> {Math.round(humidity)}</span>{" "}
+                <strong> %</strong>
               </li>
               <li>
-                Wind: <span className="wind"> </span> <strong>7 km/h</strong>
+                Wind: <span className="wind"> </span> <strong>{Math.round(wind)} km/h</strong>
               </li>
               <li>
-                Clouds: <span className="clouds"> </span> <strong>0 %</strong>
+                Clouds: <span className="clouds"> </span> <strong> %</strong>
               </li>
             </ul>
           </div>
@@ -59,3 +81,8 @@ export default function Currentforecast() {
     </div>
   );
 }
+
+ function handleSubmit(event) {
+    event.preventDefault();
+    
+    }
